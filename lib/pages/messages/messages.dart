8f4cb23e7ee40ca24dae9583535bd7b8c69597b2
xiekun1992 <xkfront@outlook.com>
2020-08-11 00:56:56 +0800

    show delete menu on long pressing

--- lib/pages/messages/messages.dart
+++ lib/pages/messages/messages.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:uchat/components/TopBarView.dart';
 import 'package:uchat/pages/chat/chat.dart';
@@ -12,6 +13,7 @@ class Messages extends StatefulWidget {
 
 class _MessagesState extends State<Messages>
     with AutomaticKeepAliveClientMixin {
+  TapDownDetails _tapDownDetails;
   @override
   bool get wantKeepAlive => true;
 
@@ -21,106 +23,123 @@ class _MessagesState extends State<Messages>
     // TODO: implement build
     return TopBarView(
       title: 'uchat',
-      page: ListView.builder(
-        padding: EdgeInsets.only(top: 0),
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return InkWell(
-            onTap: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (BuildContext context) {
-                return Chat(title: '这是一条信息 ${index}');
-              }));
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border(
-                        bottom: BorderSide(
-                            style: BorderStyle.solid,
-                            color: Color(0xdddddddd),
-                            width: 1))),
-                child: Row(
-                  children: <Widget>[
-                    Stack(
-                      overflow: Overflow.visible,
-                      children: <Widget>[
-                        Padding(
-                          padding:
-                              EdgeInsets.only(top: 10, bottom: 10, right: 10),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.all(Radius.circular(5)),
-                            child: Image(
-                              width: 50,
-                              height: 50,
-                              fit: BoxFit.cover,
-                              image: AssetImage('assets/images/hotel1.jpg'),
+      page: Stack(children: <Widget>[
+        ListView.builder(
+          padding: EdgeInsets.only(top: 0),
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTapDown: (TapDownDetails detail) {
+                _tapDownDetails = detail;
+              },
+              onLongPress: () {
+                final RenderBox overlay =
+                    Overlay.of(context).context.findRenderObject();
+                showMenu(
+                    context: context,
+                    position: RelativeRect.fromRect(
+                        _tapDownDetails.globalPosition & Size(0, 0),
+                        Offset.zero & overlay.size),
+                    items: <PopupMenuEntry>[PopupMenuItem(child: Text('删除'))]);
+              },
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (BuildContext context) {
+                  return Chat(title: '这是一条信息 ${index}');
+                }));
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(
+                              style: BorderStyle.solid,
+                              color: Color(0xdddddddd),
+                              width: 1))),
+                  child: Row(
+                    children: <Widget>[
+                      Stack(
+                        overflow: Overflow.visible,
+                        children: <Widget>[
+                          Padding(
+                            padding:
+                                EdgeInsets.only(top: 10, bottom: 10, right: 10),
+                            child: ClipRRect(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                              child: Image(
+                                width: 50,
+                                height: 50,
+                                fit: BoxFit.cover,
+                                image: AssetImage('assets/images/hotel1.jpg'),
+                              ),
                             ),
                           ),
-                        ),
-                        Positioned(
-                          top: 5,
-                          right: 5,
-                          child: Container(
-                            width: 20,
-                            height: 20,
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                                color: Colors.red,
-                                borderRadius: BorderRadius.circular(10)),
-                            child: Text(
-                              '2',
+                          Positioned(
+                            top: 5,
+                            right: 5,
+                            child: Container(
+                              width: 20,
+                              height: 20,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Text(
+                                '2',
+                                style: TextStyle(
+                                    fontSize: 12, color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              '这是一条信息 ${index}',
                               style:
-                                  TextStyle(fontSize: 12, color: Colors.white),
+                                  TextStyle(color: Colors.black, fontSize: 16),
                             ),
-                          ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              '这是一条信息的具体内容，这是一条信息的具体内容',
+                              style: TextStyle(
+                                  color: Colors.black45, fontSize: 14),
+                              overflow: TextOverflow.ellipsis,
+                            )
+                          ],
                         ),
-                      ],
-                    ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            '这是一条信息 ${index}',
-                            style: TextStyle(color: Colors.black, fontSize: 16),
-                          ),
-                          SizedBox(
-                            height: 8,
-                          ),
-                          Text(
-                            '这是一条信息的具体内容，这是一条信息的具体内容',
-                            style:
-                                TextStyle(color: Colors.black45, fontSize: 14),
-                            overflow: TextOverflow.ellipsis,
-                          )
-                        ],
                       ),
-                    ),
-                    Container(
-                      width: 60,
-                      alignment: Alignment.topRight,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            '10:40',
-                            style: TextStyle(color: Colors.black54),
-                          ),
-                          SizedBox(
-                            height: 30,
-                          ),
-                        ],
+                      Container(
+                        width: 60,
+                        alignment: Alignment.topRight,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              '10:40',
+                              style: TextStyle(color: Colors.black54),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
-        },
-      ),
+            );
+          },
+        ),
+      ]),
     );
   }
 }
